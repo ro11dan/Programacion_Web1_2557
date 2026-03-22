9d7580d3332bf835775e65ef13815d34bc2ae16e
ROLLDAN <rolldan2010@gmail.com>
2026-03-21 22:10:53 -0600

    Primeros ejercicios en php

---
+++ 17032026/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulario</title>
+</head>
+<body>
+    <form action="logica.php" method="get">
+        <label for="">Nombre</label>
+        <input type="text" maxlength="35" name="nombre">
+        <br>
+        <label for="">Primer Apellido</label>
+        <input type="text" name="1Apellido">
+        <br>
+        <label for="">Segundo Apellido</label>
+        <input type="text" name="2Apellido">
+        <br>
+        <label for="">No.Cuenta</label>
+        <input type="text" name="cuenta">
+        <br>
+    <button type="submit">Enviar</button>
+     </form>
+</body>
+</html>
---
+++ 17032026/logica.php
@@ -0,0 +1,15 @@
+<?php
+$nombreUsuario=$_GET['nombre'];
+echo "Mi nombre es: $nombreUsuario<br>";
+
+$PrimerApellido=$_GET['1Apellido'];
+echo "Mi primer apellido es: $PrimerApellido<br>";
+
+$SegundoApellido=$_GET['2Apellido'];
+echo "Mi segundo apelldio es: $SegundoApellido<br>";
+
+$cuenta=$_GET['cuenta'];
+echo "Mi numero de cuenta es: $cuenta<br>";
+
+echo "gracias profe, estoy aprendiendo";
+
